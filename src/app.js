d586783fa0ab081d115a9f6b3c485afa18708b8e
Scott Appleton <scottappleton09@gmail.com>
2019-11-29 23:28:36 -0800

    Require mongoose connection into app.js express server, create /users post endpoint and create new user using User model

--- src/app.js
+++ src/app.js
@@ -1,16 +1,40 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+require('./db/mongoose');
+
+
+const User = require('./models/user');
+
+
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.post('/tasks', (request, response) => {
+app.use(express.json());
 
-})
+app.post('/users', (request, response) => {
+
+  const user = new User(request.body);
+
+  user.save().then(() => {
+    console.log('New user saved');
+    response.status(201).send('New user resource created');
+  }).catch((error) => {
+    console.log(error);
+  })
+});
 
 
 app.get('/', (request, response) => {
   response.send('Hey im the response')
+})
+
+app.post('/tasks', (request, response) => {
+
 })
 
 
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -14,45 +14,6 @@ mongoose.connect(`${process.env.MONGO_URI}`, {
   }
 })
 
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if(!validator.isEmail(value)) {
-        throw new Error('Must provide a valid email');
-      }
-    }
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age must be a positive number');
-      }
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value) {
-      if (value.toLowerCase().includes('password')) {
-        throw new Error('Password can not contain the word password')
-      }
-    }
-  }
-})
-
 const Task = mongoose.model('Task', {
   description: {
     type: String,

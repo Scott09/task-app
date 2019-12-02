b4f3f453341473246404d2ccff837413aa76fee0
Scott Appleton <scottappleton09@gmail.com>
2019-12-02 02:10:19 -0800

    Incorporate bcrypt into the application. Use middleware to hash password before saving into the database

--- src/routers/user.js
+++ src/routers/user.js
@@ -70,7 +70,15 @@ router.patch('/users/:id', async (request, response) => {
 
 
   try {
-    const user = await User.findByIdAndUpdate(request.params.id, request.body, { new: true , runValidators: true });
+
+    const user = await User.findById(request.params.id);
+
+    updates.forEach((update) => user[update] = request.body[update])
+
+    await user.save()
+
+
+    // const user = await User.findByIdAndUpdate(request.params.id, request.body, { new: true , runValidators: true });
 
     if (!user) {
       return response.status(404).send('No user found');

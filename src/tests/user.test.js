9483562a552d9e3d4f65cbee37cef75c1d05034f
Scott Appleton <scottappleton09@gmail.com>
2019-12-11 14:33:18 -0800

    Refactor test user data to now include an id which we use to create our jsonwebtoken. Add Jest test to get user profile. Setting the Authorization header to be the jwt token

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,11 +1,19 @@
 const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const app = require('../app');
 const User = require('../models/user');
 
+
+const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
+  "_id": userOneId,
   "name": "Mike",
   "email": "mike@test.com",
-  "password": "1234567"
+  "password": "1234567",
+  tokens: [{
+    token: jwt.sign({ _id: userOneId }, 'thisismysecret')
+  }]
 }
 
 beforeEach(async () => {
@@ -39,6 +47,14 @@ test('Testing if user login information is incorrect', async () => {
     "email": "mike@test.com",
     "password": "12345678"
   }).expect(400)
+});
+
+test('Should get user profile', async () => {
+  await request(app)
+  .get('/users/me')
+  .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+  .send()
+  .expect(200)
 })
 
 

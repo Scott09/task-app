df276a1eb1730af2849d2ef19b298f152ec251c6
Scott Appleton <scottappleton09@gmail.com>
2019-12-20 03:21:22 -0800

    Add second user to test database

--- src/tests/fixtures/db.js
+++ src/tests/fixtures/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
-const User = require('../../models/user')
-
+const User = require('../../models/user');
+const Task = require('../../models/task');
 
 const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
@@ -14,13 +14,27 @@ const userOne = {
   }]
 }
 
+const userTwoId = new mongoose.Types.ObjectId();
+const userTwo = {
+  "_id": userTwoId,
+  "name": "Andrew",
+  "email": "Andrew@test.com",
+  "password": "12345",
+  tokens: [{
+    token: jwt.sign({ _id: userTwoId }, 'thisismysecret')
+  }]
+}
+
 const setupDatabase = async () => {
   await User.deleteMany();
   await new User(userOne).save();
+  await new User(userTwo).save();
 }
 
 module.exports = {
   userOneId,
   userOne,
+  UserTwoId,
+  UserTwo,
   setupDatabase
 }
